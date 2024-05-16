dfadfe3e76f6511d171d8f28db5b2a95cb117750
BuildTools <unconfigured@null.spigotmc.org>
2024-05-15 19:53:27 -0500

    added GameDebugLogger

---
+++ Main/DebugUtils/GameDebugLogger.cs
@@ -0,0 +1,15 @@
+﻿namespace Main;
+
+internal static class GameDebugLogger
+{
+    private static BufferedStringArray _logs = new BufferedStringArray(1000);
+
+    public static void WriteLogs(IEnumerable<string> inputStrings) => 
+        _logs.WriteStrings(inputStrings);
+
+    public static void WriteLog(string input) =>
+        _logs.WriteString(input);
+
+    public static string[] ReadLogs(int size) => 
+        _logs.ReadTopStrings(size);
+}
